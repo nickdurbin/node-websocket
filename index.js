9a73d960858870b63ab6aea9a600afd2fed1387e
ndurbin <nick.durbin@gmail.com>
2020-01-14 07:20:16 -0500

    Added the socket listener for the open chat

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
 
 const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, function() {
   console.log(`Server listening on PORT: ${PORT}`)
-})
+});
 
 // Static files
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+// Socket setup
+
+const io = socket(server);
+
+io.on('connection', function(socket) {
+  console.log("Made socket connection.", socket.id)
+  
+  socket.on('chat', function(data) {
+    io.sockets.emit('chat', data);
+  });
+});
