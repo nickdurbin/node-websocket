881d9073573188b139860e73a00c08f595f3397a
ndurbin <nick.durbin@gmail.com>
2020-01-14 07:32:20 -0500

    Added broadcast  to the chat app.

--- public/chat.js
+++ public/chat.js
@@ -8,6 +8,7 @@ const message = document.getElementById("message")
 const handle = document.getElementById("handle")
 const btn = document.getElementById("send")
 const output = document.getElementById("output")
+const feedback = document.getElementById("feedback")
 
 // Emit Events
 
@@ -18,8 +19,17 @@ btn.addEventListener('click', function() {
   })
 })
 
+message.addEventListener('keypress', function() {
+  socket.emit('typing', handle.value)
+})
+
 //Listen for events
 
 socket.on('chat', function(data) {
+  feedback.innerHTML = ""
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
+
+socket.on('typing', function(data) {
+  feedback.innerHTML = '<p><em>' + data + 'is typing a message...</em></p>'
 })
